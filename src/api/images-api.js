7146b7ddfc8d6c9e93061838e151be4f7b3ee714
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2021-07-27 11:49:59 +0300

    bugfix axios

--- src/api/images-api.js
+++ src/api/images-api.js
@@ -1,12 +1,21 @@
 import axios from 'axios';
 
-const API_KEY = '21915448-ca2a23b845d7ec90c85800139';
-const BASE_URL = 'https://pixabay.com/api';
+axios.defaults.baseURL = 'https://pixabay.com/api';
+axios.defaults.params = {
+  key: '21915448-ca2a23b845d7ec90c85800139',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  per_page: 12,
+};
 
 const fetchImages = async (searchQuery, page) => {
   try {
-    const url = `${BASE_URL}/?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
-    const { data } = await axios.get(url);
+    const { data } = await axios.get('', {
+      params: {
+        q: searchQuery,
+        page,
+      },
+    });
     return data.hits;
   } catch (error) {
     return [];
